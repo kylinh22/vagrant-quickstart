7f9f386f6bbb4b2b6f15cbd9907b8a426f3edec5
ky vu <kyvmm@aol.com>
2019-01-29 21:30:01 -0500

    UPdates for Tomcat 8 Bash shell provisioning

--- provision-tomcat.sh
+++ provision-tomcat.sh
@@ -8,7 +8,7 @@ cd /usr/local
 wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.0.30/bin/apache-tomcat-8.0.30.tar.gz
 tar -xzvf apache-tomcat-8.0.30.tar.gz  
 ln -s apache-tomcat-8.0.30 tomcat
-cp /vagrant/tomcat8 /etc/init.d/tomcat8
+cp /vagrant/projects/vagrant-quickstart/tomcat8 /etc/init.d/tomcat8
 chmod 755 /etc/init.d/tomcat8
 update-rc.d tomcat8 defaults
 service tomcat8 start
